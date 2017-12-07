a1c716a520053c7c246e5efe66c642da8e347981
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-07 13:59:23 +0000

    Will that connect work ?

---
+++ connect.php
@@ -0,0 +1,50 @@
+<?php
+
+if( getenv( "VCAP_SERVICES" ) )
+{
+    # Get database details from the VCAP_SERVICES environment variable
+    #
+    # *This can only work if you have used the Bluemix dashboard to
+    # create a connection from your dashDB service to your PHP App.
+    #
+    $details  = json_decode( getenv( "VCAP_SERVICES" ), true );
+    $dsn      = $details [ "dashDB For Transactions" ][0][ "credentials" ][ "dsn" ];
+    $ssl_dsn  = $details [ "dashDB For Transactions" ][0][ "credentials" ][ "ssldsn" ];
+
+    # Build the connection string
+    #
+    $driver = "DRIVER={IBM DB2 ODBC DRIVER};";
+    $conn_string = $driver . $dsn;     # Non-SSL
+    $conn_string = $driver . $ssl_dsn; # SSL
+
+//    $conn = db2_connect( $conn_string, "", "" );
+$conn=true;
+    if( $conn )
+    {
+        $_SESSION['conn'] = $conn;
+        $schema = isset($_SESSION['Db2Schema']) ? $_SESSION['Db2Schema'] : 'REST';
+        $Statement = "SET CURRENT SCHEMA='$schema';";
+        $rs = db2_exec($conn, $Statement);
+
+        if (! $rs) {
+            echo "<br/>" . $Statement . "<br/>";
+            echo "<BR>" . db2_stmt_errormsg() . "<BR>";
+            echo "<BR>" . db2_stmt_error() . "<BR>";
+            exit("Set current schema failed");
+        }
+        db2_autocommit($conn, TRUE); // This is how it was on the Wintel Box - so the code has no/few commit points.
+    }
+    else
+    {
+        echo "<p>Connection failed.</p>";
+    }
+}
+else
+{
+    echo "<p>No credentials.</p>";
+}
+
+
+
+
+?>
