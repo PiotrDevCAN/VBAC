e5735f27b476970a5f1d5a4d13c446749e5a379a
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-07 15:55:39 +0000

    remove btn group

--- index.php
+++ index.php
@@ -15,10 +15,8 @@ body {
 <div class="container">
 	<div class="jumbotron">
 		<h1 id='welcomeJumotron'>Ventus Boarding & Access Control</h1>
-		<div class='btn-group'>
 		<button type='button' class='btn btn-default'><span class="glyphicon glyphicon-log-in"></span>&nbsp;On Boarding</button>
 		<button type='button' class='btn btn-default'><span class="glyphicon glyphicon-log-out"></span>&nbsp;Off Boarding</button>
-		</div>
 	</div>
 </div>
 <!-- /.container-fluid -->
