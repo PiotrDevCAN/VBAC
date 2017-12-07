b1a2ca583a5965f4a2be3fb1e037179bd9c0743e
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-07 17:18:06 +0000

    vBAC

--- php/menuconf.php
+++ php/menuconf.php
@@ -1,6 +1,6 @@
 <?php
 $navBarImage = ""; //a small image to displayed at the top left of the nav bar
-$navBarBrand = array(strtoupper($_SERVER['environment']),"index.php");
+$navBarBrand = array(lcfirst(strtoupper($_SERVER['environment'])),"index.php");
 $navBarSearch = false;
 
 
