31e62fbc7fa25e837d0f05bba7bdef282e563865
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-07 17:20:40 +0000

    Both buttons

--- index.php
+++ index.php
@@ -16,8 +16,7 @@ body {
 	<div class="jumbotron">
 		<h1 id='welcomeJumotron'>Ventus Boarding & Access Control</h1>
 	</div>
-	<button type='button' class='btn btn-default' id='onBoardingBtn'><a href="pon_individual.php">
-	<span class="glyphicon glyphicon-log-in"></span>&nbsp;On Boarding</a></button>
-	<button type='button' class='btn btn-default' id='offBoardingBtn><span class="glyphicon glyphicon-log-out"></span>&nbsp;Off Boarding</button>
+	<button type='button' class='btn btn-default' id='onBoardingBtn'><a href='pon_individual.php'><span class="glyphicon glyphicon-log-in"></span>&nbsp;On Boarding</a></button>
+	<button type='button' class='btn btn-default' id='offBoardingBtn'><a href='poff_individual.php'><span class="glyphicon glyphicon-log-out"></span>&nbsp;Off Boarding</a></button>
 
 </div>
