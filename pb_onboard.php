e5d9ccf2c53649bad3b1143a2aa9b811288f06a3
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-13 10:58:19 +0000

    Align heading with Table

--- pb_onboard.php
+++ pb_onboard.php
@@ -7,12 +7,20 @@ set_time_limit(0);
 //Trace::pageOpening($_SERVER['PHP_SELF']);
 ?>
 <div class='container'>
+<div class='row'>
+<div class='col-sm-2'></div>
+<div class='col-sm-8'>
 <h2>Onboard Individual</h2>
+</div>
+<div class='col-sm-2'></div>
+</div>
+<div class='row'>
 <?php
 $mode = FormClass::$modeDEFINE;
 $person = new personRecord();
 $person->displayBpDetails($mode);
 ?>
+</div>
 
 
 </div>
