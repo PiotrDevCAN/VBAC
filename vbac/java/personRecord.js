02fdcf188b66f34c83c9b31cc44daef39052b1bd
Rob Daniel <rob.daniel@uk.ibm.com>
2018-01-04 15:02:32 +0000

    Improved Sort order on PES report

--- vbac/java/personRecord.js
+++ vbac/java/personRecord.js
@@ -308,8 +308,9 @@ function personRecord() {
 	this.listenForReportPes = function(){
 		$(document).on('click','#reportPes', function(e){
 			personRecord.table.columns().visible(true,false);
-			personRecord.table.columns([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]).visible(false);
+			personRecord.table.columns([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]).visible(false);
 			personRecord.table.columns([4,5,21,22,23,24,25]).visible(true);
+			personRecord.table.order([21,'desc'],[5,"asc"]);
 			personRecord.table.columns.adjust().draw(false);
 			});
 	},
