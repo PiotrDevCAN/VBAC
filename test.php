1b1641a34ddd0ccda528ba802717b5253dad5e87
Rob Daniel <rob.daniel@uk.ibm.com>
2018-01-08 17:04:39 +0000

    more bluegroups diags

--- test.php
+++ test.php
@@ -1,7 +1,20 @@
 <?php
+echo "<pre>";
 
 var_Dump($GLOBALS['ltcuser']['mail']);
 
 $cdi = employee_bluegroups($GLOBALS['ltcuser']['mail']);
 
+
 var_dump($cdi);
+
+$isCdi = employee_in_group($_SESSION['cdiBg'], $GLOBALS['ltcuser']['mail']);
+$isPmo = employee_in_group($_SESSION['pmoBg'], $GLOBALS['ltcuser']['mail']);
+
+var_dump($isCdi);
+
+var_dump($isPmo);
+
+echo "</pre>";
+
+phpinfo();
