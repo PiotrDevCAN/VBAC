51dd549696708eee75c75ef526f604753331e763
Rob Daniel <rob.daniel@uk.ibm.com>
2018-01-11 14:40:53 +0000

    Improved continer layout

--- pb_onboard.php
+++ pb_onboard.php
@@ -6,22 +6,18 @@ set_time_limit(0);
 
 //Trace::pageOpening($_SERVER['PHP_SELF']);
 ?>
+
 <div class='container'>
-<div class='row'>
 <div class='col-sm-2'></div>
 <div class='col-sm-8'>
 <h2>Onboard Individual</h2>
-</div>
-<div class='col-sm-2'></div>
-</div>
-<div class='row'>
 <?php
 $mode = personRecord::$modeDEFINE;
 $person = new personRecord();
 $person->displayBoardingForm($mode);
-
 ?>
 </div>
+<div class='col-sm-2'></div>
 </div>
 <?php
 $person->savingBoardingDetailsModal();
