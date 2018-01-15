0cb4db721d86694d7fb1fb64b87f69c08ec9b4f5
Rob Daniel <rob.daniel@uk.ibm.com>
2018-01-15 13:01:10 +0000

    config SSO for UT

--- cfg/openidconnect.inc.php
+++ cfg/openidconnect.inc.php
@@ -8,6 +8,11 @@ $config_openidconnect->client_id['vbac'] = "NjVhM2FkZjQtNmU2Ny00";
 $config_openidconnect->client_secret['vbac'] = "MzJkYjc5YjktNjBhYy00";
 $config_openidconnect->redirect_url['vbac'] = "https://vbac.w3ibm.mybluemix.net/auth/index.php";
 
+
+$config_openidconnect->client_id['vbac-ut'] = "ZTRmZmVlMWQtYWI0Mi00";
+$config_openidconnect->client_secret['vbac-ut'] = "OGI5MzQwYTMtNzViMS00";
+$config_openidconnect->redirect_url['vbac-ut'] = "https://vbac-ut.w3ibm.mybluemix.net/auth/index.php";
+
 // $config_openidconnect->client_id['rob_dev'] = "ZGE0NDYzMTctYmZhNS00";
 // $config_openidconnect->client_secret['rob_dev'] = "ZWFhM2U5MTktNDk4NS00";
 // $config_openidconnect->redirect_url['rob_dev'] = "https://restdev.w3ibm.mybluemix.net/auth/index.php";
