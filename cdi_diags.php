867545755614cc6ee63f5b86767cd99a60f1345f
Rob Daniel <rob.daniel@uk.ibm.com>
2018-01-15 10:27:03 +0000

    Remove diags

--- cdi_diags.php
+++ cdi_diags.php
@@ -11,24 +11,13 @@ unset($_SESSION['isCdi']);
 unset($_SESSION['isPmo']);
 unset($_SESSION['isUser']);
 
-echo "call is manager";
-
 $isFm   = personTable::isManager($GLOBALS['ltcuser']['mail'])                 ? ".not('.accessFm')" : null;
-var_dump($isFm);
-var_dump($GLOBALS['ltcuser']['mail']);
-
-echo "is manager over";
 
 
 
 $isCdi  = employee_in_group($_SESSION['cdiBg'],  $GLOBALS['ltcuser']['mail']) ? ".not('.accessCdi')" : null;
-//$isPmo  = employee_in_group($_SESSION['pmoBg'],  $GLOBALS['ltcuser']['mail']) ? ".not('.accessPmo')" : null;
+$isPmo  = employee_in_group($_SESSION['pmoBg'],  $GLOBALS['ltcuser']['mail']) ? ".not('.accessPmo')" : null;
 
-$isPmo = ".not('.accessPmo')";
-$isUser = ".not('.accessUser')";
-
-// $isCdi   = stripos($_SERVER['environment'], 'dev') ? ".not('.accessCdi')"  : $isCdi;
-// $isPmo   = stripos($_SERVER['environment'], 'dev')  ? ".not('.accessPmo')" : $isPmo;
 
 $_SESSION['isFm']   = !empty($isFm)   ? true : false;
 $_SESSION['isCdi']  = !empty($isCdi)  ? true : false;
