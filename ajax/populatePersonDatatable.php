a0e6e0f6f4c875ef89c951ac8e38e0e1b7e82310
Rob Daniel <rob.daniel@uk.ibm.com>
2018-01-25 17:05:20 +0000

    Put the code back as it should be

--- ajax/populatePersonDatatable.php
+++ ajax/populatePersonDatatable.php
@@ -11,20 +11,15 @@ ob_start();
 $personTable = new personTable(allTables::$PERSON);
 $data = $personTable->returnAsArray();
 
-var_dump($data);
-
-
 $dataJsonAble = json_encode($data);
-
-var_dump($dataJsonAble);
 
 $messages = ob_get_clean();
 
-// if(!$dataJsonAble) {
-//     $response = array("data"=>$data,'messages'=>$messages);
-// } else {
-    $response = array("data"=>'error','messages'=>$messages);
+if($dataJsonAble) {
+     $response = array("data"=>$data,'messages'=>$messages);
+ } else {
+    $response = array("data"=>'error, data can\'t be JSON ENcoded, look for special chars','messages'=>$messages);
 
-// }
+ }
 ob_clean();
 echo json_encode($response);
