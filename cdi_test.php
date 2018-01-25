7827234261c167afe380dc0634ee6b9a16e0e55b
Rob Daniel <rob.daniel@uk.ibm.com>
2018-01-25 16:43:00 +0000

    this is tricky

--- cdi_test.php
+++ cdi_test.php
@@ -3,23 +3,29 @@
 use itdq\Loader;
 use vbac\allTables;
 
+$personTable = new personTable(allTables::$PERSON);
+$data = $personTable->returnAsArray();
 
-$CO = 'GB';
-
-$loader = new Loader();
-$countryName = $loader->loadIndexed('COUNTRY_NAME','COUNTRY_CODE',allTables::$STATIC_COUNTRY_CODES, " COUNTRY_CODE='" . db2_escape_string(trim($CO)) . "' ");
-
-var_dump($countryName);
-
-var_dump(isset($countryName[$CO]));
+var_dump($data);
 
 
-$CO = 'XX';
 
-$loader = new Loader();
-$countryName = $loader->loadIndexed('COUNTRY_NAME','COUNTRY_CODE',allTables::$STATIC_COUNTRY_CODES, " COUNTRY_CODE='" . db2_escape_string(trim($CO)) . "' ");
+// $CO = 'GB';
 
-var_dump($countryName);
-var_dump(isset($countryName[$CO]));
+// $loader = new Loader();
+// $countryName = $loader->loadIndexed('COUNTRY_NAME','COUNTRY_CODE',allTables::$STATIC_COUNTRY_CODES, " COUNTRY_CODE='" . db2_escape_string(trim($CO)) . "' ");
+
+// var_dump($countryName);
+
+// var_dump(isset($countryName[$CO]));
+
+
+// $CO = 'XX';
+
+// $loader = new Loader();
+// $countryName = $loader->loadIndexed('COUNTRY_NAME','COUNTRY_CODE',allTables::$STATIC_COUNTRY_CODES, " COUNTRY_CODE='" . db2_escape_string(trim($CO)) . "' ");
+
+// var_dump($countryName);
+// var_dump(isset($countryName[$CO]));
 
 
