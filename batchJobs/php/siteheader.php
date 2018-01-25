a553a09b99f1444c4124653b76a2b1c996faa260
Rob Daniel <rob.daniel@uk.ibm.com>
2018-01-25 12:49:41 +0000

    Move SessionStart

--- batchJobs/php/siteheader.php
+++ batchJobs/php/siteheader.php
@@ -4,18 +4,13 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 date_default_timezone_set('UTC');
 set_include_path("./" . PATH_SEPARATOR . "../" . PATH_SEPARATOR . "../../" . PATH_SEPARATOR . "../../../" . PATH_SEPARATOR);
-
-include "../php/w3config.php";
-
-$_SESSION['Db2Schema'] = $_SERVER['environment'];
-
-
-include "vendor/autoload.php";
-include "splClassLoader.php";
 session_start();
 
-include "connect.php";
+include "../php/w3config.php";
+include "vendor/autoload.php";
+include "splClassLoader.php";
 
+include "connect.php";
 
 var_dump($_SESSION);
 var_dump($_SERVER);
