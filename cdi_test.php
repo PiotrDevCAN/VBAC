2a34eb2f415ebb448a50476d60e69839f873d23f
Rob Daniel <rob.daniel@uk.ibm.com>
2018-02-08 10:21:39 +0000

    Check $_SERVER

--- cdi_test.php
+++ cdi_test.php
@@ -1,32 +1,7 @@
 <?php
 
-use itdq\Loader;
-use vbac\allTables;
-use vbac\personTable;
-
-$personTable = new personTable(allTables::$PERSON);
-$data = $personTable->returnAsArray();
-
-var_dump($data);
-
-
-
-// $CO = 'GB';
-
-// $loader = new Loader();
-// $countryName = $loader->loadIndexed('COUNTRY_NAME','COUNTRY_CODE',allTables::$STATIC_COUNTRY_CODES, " COUNTRY_CODE='" . db2_escape_string(trim($CO)) . "' ");
-
-// var_dump($countryName);
-
-// var_dump(isset($countryName[$CO]));
-
-
-// $CO = 'XX';
-
-// $loader = new Loader();
-// $countryName = $loader->loadIndexed('COUNTRY_NAME','COUNTRY_CODE',allTables::$STATIC_COUNTRY_CODES, " COUNTRY_CODE='" . db2_escape_string(trim($CO)) . "' ");
-
-// var_dump($countryName);
-// var_dump(isset($countryName[$CO]));
-
+?><pre>
+<?=print_r($_SERVER)?>
+</pre>
+<?php
 
