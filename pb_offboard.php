1abc2130ad20e7d77551201f829b693937038758
Rob Daniel <rob.daniel@uk.ibm.com>
2018-03-07 11:57:13 +0000

    Add text to Offboarding Screen to tell mgrs to use Person Portal Improved wording on offboarding.

--- pb_offboard.php
+++ pb_offboard.php
@@ -2,7 +2,19 @@
 ?>
 <div class='container'>
 <h2>Offboard Individual</h2>
-<p>This functionality is under development</p>
+<p><b>vBAC Offboarding functionality is under development.</b></p>
+
+<p>A Functional (Line) Manager can still flag the requirement to offboard a person from the Venus account by updating the "End Date" for the individual in their boarding record.</p>
+<p>[vBAC Admin > <a href='/pa_pmo.php'>Person Portal</a> > Edit by clicking on the icon against the NotesID field]</p>
+
+<p>If an individuals end date is updated into the past or to today's date then an email is sent to the PMO task id. The PMO will actively offboard any individuals when their end date is reached.
+<p>A list of your reportees who will be offboarded within the next  30 days can be found in the offboarding report in the person portal.</p>
+<p>[vBAC Admin > Person Portal > Offboarding report button]</p>
+
+<p>If this is not correct then update the individuals "End date" in their boarding record per the instructions above. Once an individual is marked as "offboarded" by the PMO that person will disappear from the offboarding report.</p>
+
+<p>For audit purposes the individual will continue to appear in your list of employees but with a validation status of "Offboarded".
+Please contact the Central PMO for more information: <b>Aurora Central PMO/UK/IBM</b></p>
 </div>
 <?php
 
