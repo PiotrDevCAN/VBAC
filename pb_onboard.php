61b094b0c6acd37436241e55eafb572f6d19e15b
Rob Daniel <rob.daniel@uk.ibm.com>
2018-03-26 10:55:51 +0100

    Change label on Boarding Buttton

--- pb_onboard.php
+++ pb_onboard.php
@@ -14,7 +14,7 @@ set_time_limit(0);
 <div class='col-sm-2'></div>
 <div class='col-sm-8'>
 <h2>Onboard Individual
-<input checked data-size="mini" data-toggle="toggle" type="checkbox" class='toggle' data-width='100' data-on="Boarding" data-off="Pre-Boarding" id='hasBpEntry'>
+<input checked data-size="mini" data-toggle="toggle" type="checkbox" class='toggle' data-width='100' data-on="IBMer Boarding" data-off="Pre-Hire" data-onstyle="primary" data-offstyle="warning" id='hasBpEntry'>
 </h2>
 </div>
 </div>
