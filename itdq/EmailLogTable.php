43bf2e0b935f665230b1fb967c4186b32b482947
Rob Daniel <rob.daniel@uk.ibm.com>
2018-04-17 16:53:25 +0100

    Keep emails longer

--- itdq/EmailLogTable.php
+++ itdq/EmailLogTable.php
@@ -8,7 +8,7 @@ class EmailLogTable  extends DbTable {
         $now = new \DateTime();
 
         $endDate = isset($endDate) ? $endDate : $now->format('Y-m-d');
-        $startDate = isset($startDate) ? $startDate : date_format($now->modify("-1 week"),'Y-m-d');
+        $startDate = isset($startDate) ? $startDate : date_format($now->modify("-1 month"),'Y-m-d');
 
         $sql  = " SELECT * FROM " . $_SESSION['Db2Schema'] . "." . AllItdqTables::$EMAIL_LOG;
         $sql .= " WHERE DATE(SENT_TIMESTAMP) >= DATE('" . db2_escape_string($startDate) . "') and DATE(SENT_TIMESTAMP) <= DATE('" . db2_escape_string($endDate) . "') ";
