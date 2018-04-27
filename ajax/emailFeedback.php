3e28c60727b243c6146676609e259731b5027cdf
Rob Daniel <rob.daniel@uk.ibm.com>
2018-04-27 16:43:26 +0100

    correct email address

--- ajax/emailFeedback.php
+++ ajax/emailFeedback.php
@@ -2,7 +2,8 @@
 
 use itdq\BlueMail;
 
-$sendResponse = BlueMail::send_mail(array('rob.daniel@uk.ibm.com'), 'vBAC Feedback', $_POST['feedback'],$_POST['sender']);
-
+if(!empty($_POST['feedback'])){
+    $sendResponse = BlueMail::send_mail(array('lbgrep@uk.ibm.com'), 'vBAC Feedback', $_POST['feedback'],$_POST['sender']);
+}
 ob_clean();
 
