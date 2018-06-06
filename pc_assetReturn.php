10514403591608daedad2379e194d98efc51b14d
Rob Daniel <rob.daniel@uk.ibm.com>
2018-06-06 15:20:28 +0100

    Change wording on th Return Asset page

--- pc_assetReturn.php
+++ pc_assetReturn.php
@@ -2,5 +2,6 @@
 ?>
 <div class='container'>
 <h1>Asset Return</h1>
-<p>vBAC does not suport the RETURN of a Digital Asset, please contact the PMO Team directly for the manual process.</p>
+<p>Assets that can be returned, now have an entry on the "Request Digital Assets" page.</p>
+<p>So to return an RSA Token - Select "RSA Token Return/Deactivation", to return an MPW ID - select "MPW ID Return/Deactivation" etc.</p>
 </div>
