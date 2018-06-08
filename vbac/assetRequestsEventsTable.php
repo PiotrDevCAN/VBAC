40d76285654b80be81977b6bc693f9bdcc3c460f
Rob Daniel <rob.daniel@uk.ibm.com>
2018-06-08 16:06:14 +0100

    Better define Events to be tracked.

--- vbac/assetRequestsEventsTable.php
+++ vbac/assetRequestsEventsTable.php
@@ -7,12 +7,19 @@ use vbac\allTables;
 
 class assetRequestsEventsTable extends DbTable{
 
-    const EVENT_CREATED             = 'created';
-    const EVENT_VBAC_APPROVED       = 'vbac approved';
-    const EVENT_VBAC_REJECTED       = 'vbac rejected';
-    const EVENT_EXPORTED            = 'exported';
-    const EVENT_ORDERIT_APPROVED    = 'orderit approved';
-    const EVENT_ORDERIT_REJECTED    = 'orderit rejected';
+    const EVENT_CREATED             = assetRequestRecord::$STATUS_CREATED;
+    const EVENT_VBAC_APPROVED       = assetRequestRecord::$STATUS_APPROVED;
+    const EVENT_VBAC_REJECTED       = assetRequestRecord::$STATUS_REJECTED;
+    const EVENT_EXPORTED            = assetRequestRecord::$STATUS_EXPORTED;
+    const EVENT_DEVARBED            = 'devarbed';
+    const EVENT_ORDERIT_RAISED      = assetRequestRecord::$STATUS_RAISED_ORDERIT;
+    const EVENT_ORDERIT_APPROVED    = assetRequestRecord::$STATUS_ORDERIT_APPROVED;
+    const EVENT_ORDERIT_REJECTED    = assetRequestRecord::$STATUS_ORDERIT_REJECTED;
+    const EVENT_ORDERIT_CANCELLED   = assetRequestRecord::$STATUS_ORDERIT_CANCELLED;
+    const EVENT_MISC                ='misc';
+
+    const LOG_TYPE_EVENT = 'event';
+    const LOG_TYPE_STATUS = 'status';
 
     private $preparedInsertEventStatement;
 
