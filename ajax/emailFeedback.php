0f1cbacb4234245461da0964eb36f6326daec162
Rob Daniel <rob.daniel@uk.ibm.com>
2018-07-11 13:06:35 +0100

    Changes to Help page

--- ajax/emailFeedback.php
+++ ajax/emailFeedback.php
@@ -3,7 +3,7 @@
 use itdq\BlueMail;
 
 if(!empty($_POST['feedback'])){
-    $sendResponse = BlueMail::send_mail(array('lbgrep@uk.ibm.com'), 'vBAC Feedback', $_POST['feedback'],$_POST['sender']);
+    $sendResponse = BlueMail::send_mail(array('IBM.LBG.IAM.Requests@uk.ibm.com'), 'vBAC Question', $_POST['feedback'],$_POST['sender']);
 }
 ob_clean();
 
--- pa_helpPage.php
+++ pa_helpPage.php
@@ -52,33 +52,40 @@ body {
 
 
 <div class="container">
-<!-- <div class="jumbotron"> -->
 <h1 id='welcomeJumotron'>Training Links & Feedback</h1>
-<!-- </div> -->
 
+<div class="alert alert-warning helplink">
+  <strong>Unexpected Results ? </strong>&nbsp;Please note only the <strong>Firefox</strong> browser is 100% supported for use with vBAC. Uf you experience <strong>"unexpected result"</strong> and are NOT using Firefox, then please retry using Firefox before you escalate for support.
+</div>
+
+
+<div class="panel panel-default">
+<div class='panel-body'>
 <ul class='helpLink'>
 <li><a href='https://w3.tap.ibm.com/medialibrary/media_set_view?id=47864' target='_blank' class=''>User Guide and Training Videos</a></li>
 <li><a href='http://w3.tap.ibm.com/data/medialibrary/media/419943.doc/1048424/2018_05_03_2111_vBAC_FAQs.doc' target='_blank' class=''>FAQ</a></li>
 
-<li class=' feedback'>Feedback
+
+<li class=' feedback'>Questions
 <div id='feedbackDiv'>
 <form id='feedbackForm' onsubmit='return false'>
-
 <div class='row'>
 <div class='form-group col-sm-6'>
-<textarea id='feedbackText' rows="4" cols="25" style='font-size:14px' placeholder="Type feedback or comments,  then click 'Send' below"></textarea>
+<textarea id='feedbackText' rows="4" cols="25" style='font-size:14px' placeholder="Type questions for the IAM support team then click 'Send' below. But please check FAQ first, how to resolve many issues are covered in the FAQ"></textarea>
 </div>
 </div>
 <div class='row'>
 <div class='form-group col-sm-1'>
-<button id='feedbackSend' class='btn btn-primary form-control col-sm-2'>Send</button>
+<button id='feedbackSend' class='btn btn-xs btn-primary form-control col-sm-2'>Send</button>
 </div>
 </div>
 </form>
 </div>
-<li>Find us on Slack&nbsp;<a href='https://ventusdelivery.slack.com/messages/C8DLE1DFH/' target='_blank' ># sm-cognitive-delivery</a></li>
-</li>
+<li>Contact IAM Support : <a href='mailto:IBM.LBG.IAM.Requests@uk.ibm.com'>IBM LBG IAM Requests/UK/IBM</a> with any questions regarding requests within vBAC/Process/etc</li>
+<li>Contact vBAC Application Support on Slack&nbsp;<a href='https://ventusdelivery.slack.com/messages/C8DLE1DFH/' target='_blank' ># sm-cognitive-delivery</a> if you experience a bug<br/><small>Please note: This team only support the code behind vBAC they do not support the boarding/access control processes themselves</small></li>
 </ul>
+</div>
+</div>
 </div>
 
 <<script type="text/javascript">
