8575bad95e41523aa053098a4e0e31aa0ac9b25a
Rob Daniel <rob.daniel@uk.ibm.com>
2018-08-14 13:23:44 +0100

    Sql to put new EVENT on the report

---
+++ sql/Asset_Requests_Events_Interim.sql
@@ -0,0 +1,29 @@
+--<ScriptOptions statementTerminator=";"/>
+
+DROP VIEW "ROB_DEV"."ASSET_REQUESTS_EVENTS_INTERIM";
+
+CREATE VIEW "ROB_DEV"."ASSET_REQUESTS_EVENTS_INTERIM" ("REF", "CREATED_IN_VBAC", "REJECTED_IN_VBAC", "APPROVED_FOR_ORDER_IT", "PRE_REQ_CREATED", "PRE_REQ_APPROVED", "EXPORTED_FOR_ORDER_IT", "RAISED_IN_ORDER_IT", "APPROVED_IN_ORDER_IT", "REJECTED_IN_ORDER_IT", "PROVISIONED_BY_ORDER_IT","ORDER_IT_RESPONDED") AS
+select request_reference as ref,
+case when event = 'Created in vBAC' then occured else null end  as created_in_vbac
+,case when event = 'Rejected in vBAC' then occured else null end as rejected_in_vbac
+,case when event = 'Approved for Order IT' then occured else null end as approved_for_order_it
+,case when event = 'Pre-req Created' then occured else null end as pre_req_created
+,case when event = 'Pre-req Approved' then occured else null end as pre_req_approved
+,case when event = 'Exported for Order IT' then occured else null end  as exported_for_order_it
+,case when event = 'Raised in Order IT' then occured else null end  as raised_in_order_it
+,case when event = 'Approved in Order IT' then occured else null end as approved_in_order_it
+,case when event = 'Rejected in Order IT' then occured else null end as rejected_in_order_it
+,case when event = 'Provisioned by Order IT' then occured else null end as provisioned_by_order_it
+,case when event = 'orderIt Responded ' then occured else null end as order_it_responded
+from rob_dev.asset_requests_events;
+
+GRANT CONTROL ON TABLE "ROB_DEV"."ASSET_REQUESTS_EVENTS_INTERIM" TO USER "BLUADMIN";
+
+GRANT DELETE ON TABLE "ROB_DEV"."ASSET_REQUESTS_EVENTS_INTERIM" TO USER "BLUADMIN" WITH GRANT OPTION;
+
+GRANT INSERT ON TABLE "ROB_DEV"."ASSET_REQUESTS_EVENTS_INTERIM" TO USER "BLUADMIN" WITH GRANT OPTION;
+
+GRANT SELECT ON TABLE "ROB_DEV"."ASSET_REQUESTS_EVENTS_INTERIM" TO USER "BLUADMIN" WITH GRANT OPTION;
+
+GRANT UPDATE ON TABLE "ROB_DEV"."ASSET_REQUESTS_EVENTS_INTERIM" TO USER "BLUADMIN" WITH GRANT OPTION;
+
---
+++ sql/Asset_Requests_Events_Summary.sql
@@ -0,0 +1,23 @@
+--<ScriptOptions statementTerminator=";"/>
+
+DROP VIEW "ROB_DEV"."ASSET_REQUESTS_EVENTS_SUMMARY";
+
+CREATE VIEW "ROB_DEV"."ASSET_REQUESTS_EVENTS_SUMMARY" ("REF", "CREATED_IN_VBAC", "REJECTED_IN_VBAC", "APPROVED_FOR_ORDER_IT", "PRE_REQ_CREATED", "PRE_REQ_APPROVED", "EXPORTED_FOR_ORDER_IT", "RAISED_IN_ORDER_IT", "APPPROVED_IN_ORDER_IT", "REJECTED_IN_ORDER_IT", "PROVISIONED_BY_ORDER_IT","ORDER_IT_RESPONDED") AS
+select ref, max(created_in_vbac) as created_in_vbac
+, max(rejected_in_vbac) as rejected_in_vbac
+, max(approved_for_order_it) as approved_for_order_it
+, max(pre_req_created) as pre_req_created
+, max(pre_req_approved) as pre_req_approved
+, max(exported_for_order_it) as exported_for_order_it
+, max(raised_in_order_it) as raised_in_order_it
+, max(approved_in_order_it) as appproved_in_order_it
+, max(rejected_in_order_it) as rejected_in_order_it
+, max(provisioned_by_order_it) as provisioned_by_order_it
+, max(order_it_responded) as order_it_responded
+from rob_dev.asset_requests_events_interim
+group by ref;
+
+GRANT CONTROL ON TABLE "ROB_DEV"."ASSET_REQUESTS_EVENTS_SUMMARY" TO USER "BLUADMIN";
+
+GRANT SELECT ON TABLE "ROB_DEV"."ASSET_REQUESTS_EVENTS_SUMMARY" TO USER "BLUADMIN" WITH GRANT OPTION;
+
