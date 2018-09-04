584d0744c0410cf7e6172bf085ef0738f85de7cd
Rob Daniel <rob.daniel@uk.ibm.com>
2018-09-04 15:35:46 +0100

    Don't allow app to run under IE

---
+++ php/noPage.php
@@ -0,0 +1,46 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en-US"><!--lang= is REQUIRED for V18 compliance-->
+<head>
+
+    <!--START meta data REQUIRED or V18 compliance-->
+	<meta http-equiv="Content-type" content="text/html; ">
+	<meta name="viewport" content="width=device-width,initial-scale=1, user-scalable=no">
+	<!-- NOTE user-scalable opion above is not a V18 requirement and is used only to stop scaling on mobile devices. Do NOT include by default as there are accessibility consequences-->
+	<meta charset="UTF-8">
+    <link rel="icon" href="favicon.ico">
+    <link rel="canonical" href="http://www.ibm.com/______">
+    <meta name="dcterms.date" content="YYYY-MM-DD">
+    <meta name="dcterms.rights" content="������ Copyright IBM Corp. 2015">
+    <meta name="description" content="______">
+    <meta name="geo.country" content="GB">
+    <meta name="keywords" content="______">
+    <meta name="robots" content="index,follow">
+    <!--END meta data REQUIRED or V18 compliance-->
+
+	<script	  src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<!-- <script  src="https://code.jquery.com/jquery-2.2.4.js"  integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="  crossorigin="anonymous"></script> -->
+
+	<title><?php echo $_SESSION['SITE_NAME']; ?>...	</title>
+</head>
+
+<body>
+
+
+
+<div class="container">
+<!--  <div class="jumbotron"> -->
+<h1 id='welcomeJumotron'>Ventus Boarding & Access Control</h1>
+<div>
+<p>vBAC has been coded to the ECMAScript 2015 standard. This is not supported by older browsers, such as Internet Explorer(IE).</p>
+<p>It has detected that you are using an old browser that does not support vBAC.</p>
+<p>Please switch to a current browser, such as Firefox, Chrome or Safari</p>
+</div>
+
+</div>
+
+
+</body>
+
