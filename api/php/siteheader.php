f6a852cf16d8d153bc0acee0a80f616a0f575e25
Rob Daniel <rob.daniel@uk.ibm.com>
2018-09-07 16:18:49 +0100

    move api_token to env vars

--- api/php/siteheader.php
+++ api/php/siteheader.php
@@ -8,6 +8,6 @@ set_include_path("./" . PATH_SEPARATOR . "../" . PATH_SEPARATOR . "../../" . PAT
 include "vendor/autoload.php";
 include "splClassLoader.php";
 session_start();
-$token = 'soEkCfj8zGNDLZ8yXH2YJjpehd8ijzlS';
+$token = $_SERVER['api_token'];
 $_SESSION['Db2Schema'] = $_SERVER['environment'];
 include "connect.php";
