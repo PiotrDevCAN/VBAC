61edacb62f3ab2771c0ca12a03cb4c1db423f9f7
Rob Daniel <rob.daniel@uk.ibm.com>
2018-09-18 11:53:50 +0100

    Few corrections

--- api/febTemplate.php
+++ api/febTemplate.php
@@ -10,7 +10,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST':
         // Save a template to the database
         $sql = "INSERT INTO " . $_SERVER['environment'] . "." . \vbac\allTables::$FEB_TRAVEL_REQUEST_TEMPLATES ; 
-        $sql.= " (EMAIL_ADDRESS, TITLE, TEMPLATE) VALUES ('" . db2_escape_string($_GET['email_address']) . "','" .  db2_escape_string($_GET['title']) . "','" . db2_escape_string(print_r($_GET['template'],true)) . "') ";
+        $sql.= " (EMAIL_ADDRESS, TITLE, TEMPLATE) VALUES ('" . db2_escape_string($_POST['email_address']) . "','" .  db2_escape_string($_POST['title']) . "','" . db2_escape_string(print_r($_GET['template'],true)) . "') ";
         $rs = db2_exec($_SESSION['conn'], $sql);     
 
         if(!$rs){
@@ -21,10 +21,10 @@ switch ($_SERVER['REQUEST_METHOD']) {
         break;
     case 'GET':
         switch (true) {
-            case isset($_GET['emailAddress']) && isset($_GET['title']):
+            case isset($_GET['email_address']) && isset($_GET['title']):
                 // Get a specific template
                 $sql = " SELECT TEMPLATE FROM " . "." . \vbac\allTables::$FEB_TRAVEL_REQUEST_TEMPLATES ;
-                $sql.= " WHERE EMAIL_ADDRESS='" . db2_escape_string(trim($_GET['emailAddress'])) . "' ";
+                $sql.= " WHERE EMAIL_ADDRESS='" . db2_escape_string(trim($_GET['email_address'])) . "' ";
                 $sql.= " AND TITLE='" . db2_escape_string(trim($_GET['title'])) . "' ";   
                 ob_start();
                 $rs = db2_exec($_SESSION['conn'], $sql);
@@ -39,10 +39,10 @@ switch ($_SERVER['REQUEST_METHOD']) {
                 $response['template'] = $row['TEMPLATE'];
             break;
 
-            case isset($_GET['emailAddress']):
+            case isset($_GET['email_address']):
                 // Get list of titles for this person
-                $sql = " SELECT DISTINT TITLE FROM " . "." . \vbac\allTables::$FEB_TRAVEL_REQUEST_TEMPLATES ;
-                $sql.= " WHERE EMAIL_ADDRESS='" . db2_escape_string(trim($_GET['emailAddress'])) . "' ";
+                $sql = " SELECT DISTINCT TITLE FROM " . "." . \vbac\allTables::$FEB_TRAVEL_REQUEST_TEMPLATES ;
+                $sql.= " WHERE EMAIL_ADDRESS='" . db2_escape_string(trim($_GET['email_address'])) . "' ";
                 ob_start();
                 $rs = db2_exec($_SESSION['conn'], $sql);
                 
