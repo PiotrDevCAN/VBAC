00cc72ed2ee2faed4726a0e87c943c2587b45c6c
Rob Daniel <rob.daniel@uk.ibm.com>
2018-09-27 11:33:38 +0100

    Didn't like Use Evidence

--- ajax/sendPesEmail.php
+++ ajax/sendPesEmail.php
@@ -4,7 +4,7 @@ use vbac\pesEmail;
 use vbac\personTable;
 use vbac\allTables;
 use itdq\AuditTable;
-use \Exception;
+
 ob_start();
 
 AuditTable::audit("Invoked:<b>" . __FILE__ . "</b>Parms:<pre>" . print_r($_POST,true) . "</b>",AuditTable::RECORD_TYPE_AUDIT);
