45553205bc45a09b74918cb858e30f04f0a44a36
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2018-11-30 10:04:34 +0000

    ilc Reminder

---
+++ batchJobs/ilcReminder.php
@@ -0,0 +1,10 @@
+<?php
+
+use itdq\AuditTable;
+use itdq\slack;
+
+$slack = new slack();
+
+AuditTable::audit("ilcReminder invoked.",AuditTable::RECORD_TYPE_AUDIT);
+$slack->sendMessageToChannel("<!channel> *AUTOMATED CLAIM REMINDER* - Please consider this as a reminder to submit your ILC (Claim) before the end of week business deadline. Remember your claim must accurately reflect ALL hours worked. This reminder is not an instruction to submit your Claim prior to the effort actually being performed. Thank you.", slack::CHANNEL_SM_CDI_AUDIT);
+
