7ff1e692db998d440824771b43e7e867194bed9d
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2018-12-05 14:16:06 +0000

    Improve EmployeePlus.php

--- api/employeePlus.php
+++ api/employeePlus.php
@@ -8,6 +8,10 @@ if($_REQUEST['token']!= $token){
     return;
 }
 
+$emailID = !empty($_GET['emailid']) ? $_GET['emailid'] : null;
+$notesId = !empty($_GET['notesid']) ? $_GET['notesid'] : null;
+$cnum    = !empty($_GET['cnum']) ? $_GET['cnum'] : null;
+
 $additionalFields = !empty($_REQUEST['plus']) ? explode(",", $_REQUEST['plus']) : null;
 $additionalSelect = null;
 $employees = array();
@@ -21,6 +25,9 @@ $sql = " SELECT P.NOTES_ID " . $additionalSelect;
 $sql.= " FROM " . $_SERVER['environment'] . "." . allTables::$PERSON . " AS P ";
 
 $sql.= " WHERE 1=1 AND trim(NOTES_ID) != ''  AND " . personTable::activePersonPredicate();
+$sql.= !empty($emailID) ? " AND lower(P.EMAIL_ADDRESS) = '" . db2_escape_string(strtolower($emailID)) . "'; " : null;
+$sql.= !empty($notesId) ? " AND lower(P.NOTES_ID) = '" . db2_escape_string(strtolower($notesId)) . "'; " : null;
+$sql.= !empty($cnum) ? " AND lower(P.CNUM) = '" . db2_escape_string(strtolower($cnum)) . "'; " : null;
 $sql.= " ORDER BY P.NOTES_ID ";
 
 $rs = db2_exec($_SESSION['conn'], $sql);
