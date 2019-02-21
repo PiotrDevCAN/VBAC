ce619aea0f2f135ce184587678c20326376aa36b
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2019-02-21 15:11:08 +0000

    odcAccess API send CNUM return 'Yes' or 'No'

---
+++ api/odcAccess.php
@@ -0,0 +1,27 @@
+<?php
+
+use vbac\allTables;
+
+if($_REQUEST['token']!= $token){
+    return;
+}
+
+$notesId = !empty($_GET['notesid']) ? $_GET['notesid'] : null;
+$cnum    = !empty($_GET['cnum']) ? $_GET['cnum'] : null;
+$row = false;
+
+$sql = " SELECT count(*) as FOUND ";
+$sql.= " FROM " . $_SERVER['environment'] . "." . allTables::$ODC_ACCESS_LIVE . " AS O ";
+$sql.= " WHERE 1=1 ";
+$sql.= !empty($notesId) ? " AND lower(O.OWNER_NOTES_ID) = '" . db2_escape_string(strtolower($notesId)) . "'; " : null;
+$sql.= !empty($cnum) ? " AND lower(O.OWNER_CNUM_ID) = '" . db2_escape_string(strtolower($cnum)) . "'; " : null;
+
+$rs = db2_exec($_SESSION['conn'], $sql);
+
+if($rs){
+    $row = db2_fetch_assoc($rs);
+    $found = $row['FOUND'] > 0 ? 'Yes' : 'No';
+}
+
+ob_clean();
+echo json_encode($found);
