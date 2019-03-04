6682ebbb0c7c44461262be623bf03d9b3e07a3e0
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2019-03-04 16:43:14 +0000

    toTitleCase

--- vbac/java/personRecord.js
+++ vbac/java/personRecord.js
@@ -4,6 +4,13 @@
  *
  */
 
+function toTitleCase(str) {
+	    return str.replace(/\w\S*/g, function(txt){
+	        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+	    });
+};
+
+
 function personRecord() {
 
   var table;
@@ -643,7 +650,7 @@ function personRecord() {
             	  while(regex.test(firstName) && i < value.length){
             		  firstName = value[++i];
             	  }
-               capitalizedName = person.toTitleCase(firstName);
+               capitalizedName = toTitleCase(firstName);
                var fname =  document.getElementById('person_first_name');
                fname.value = capitalizedName ;
                break;           
@@ -707,12 +714,6 @@ function personRecord() {
 
   },
   
-  this.toTitleCase = function(str) {
-	    return str.replace(/\w\S*/g, function(txt){
-	        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-	    });
-  },
-
   this.listenForCtbRtb = function(){
     $(document).on('click','.ctbRtb', function(){
       var ctbRtb = $(this).val();
