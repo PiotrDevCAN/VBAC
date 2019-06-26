1ac3de972ee30f7c0452a7c49e95c892d4e4ce02
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2019-06-26 14:59:52 +0100

    Add support for active offboarding

--- api/employees.php
+++ api/employees.php
@@ -4,6 +4,9 @@ use vbac\allTables;
 use vbac\personTable;
 use itdq\DbTable;
 
+ob_start();
+
+
 if($_REQUEST['token']!= $token){
     return;
 }
@@ -11,21 +14,25 @@ if($_REQUEST['token']!= $token){
 $sql = " SELECT P.NOTES_ID ";
 $sql.= " FROM " . $_SERVER['environment'] . "." . allTables::$PERSON . " AS P ";
 
-$sql.= " WHERE 1=1 AND trim(NOTES_ID) != ''  AND " . personTable::activePersonPredicate();
+$sql.= " WHERE 1=1 AND trim(NOTES_ID) != ''  AND ( " . personTable::activePersonPredicate() . ") ";
+$sql.= isset($_REQUEST['activeoffboarding']) ? " OR ( PES_STATUS like 'Cleared%' AND REVALIDATION_STATUS like 'offboarding%' ) "  : null;
 $sql.= " ORDER BY P.NOTES_ID ";
 
 $rs = db2_exec($_SESSION['conn'], $sql);
+$employees = array();
 
 if($rs){
-    while(($row = db2_fetch_assoc($rs))==true){         
-        $employees[] = trim($row['NOTES_ID']);        
+    while(($row = db2_fetch_assoc($rs))==true){
+        $employees[] = trim($row['NOTES_ID']);
     }
 } else {
     ob_clean();
     DbTable::displayErrorMessage($rs, 'class', 'method', $sql);
     $errorMessage = ob_get_clean();
     echo json_encode($errorMessage);
+    exit();
 }
 
 ob_clean();
-echo json_encode($employees);
+$json = json_encode($employees);
+echo $json;
