f120f93fb1823c65404facd32451e0d1026ba573
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2019-08-07 12:13:21 +0100

    Support the same SUB_PLATFORM in multiple WORKSTREAM

--- vbac/staticDataSubPlatformTable.php
+++ vbac/staticDataSubPlatformTable.php
@@ -15,21 +15,34 @@ class staticDataSubPlatformTable extends DbTable
 
     static function prepareJsonObjectForSubPlatformSelect(){
         $loader = new Loader();
-        $allSubPlatform = $loader->loadIndexed('WORK_STREAM_ID','SUB_PLATFORM',allTables::$STATIC_SUBPLATFORM);
+        // $allSubPlatform = $loader->loadIndexed('WORK_STREAM_ID','SUB_PLATFORM',allTables::$STATIC_SUBPLATFORM);
         $allWorkstreams = $loader->loadIndexed('WORKSTREAM_ID','WORKSTREAM',allTables::$STATIC_WORKSTREAMS);
-        $platformWithinStream= array();
+        $allSubPlatformsByWorkstream = array();
+         $platformWithinStream= array();
 
-        foreach ($allSubPlatform as $subPlatform => $work_stream_id) {
-            $options = new \stdClass();
-            $options->id   = trim($subPlatform);
-            $options->text = trim($subPlatform);
-            $platformWithinStream[$work_stream_id][] = $options;
+        foreach ($allWorkstreams as $workstream => $workstream_id) {
+            $predicate = " WORK_STREAM_ID='$workstream_id' ";
+            $allSubPlatformsByWorkstream[$workstream_id] = $loader->load('SUB_PLATFORM',allTables::$STATIC_SUBPLATFORM,$predicate);
         }
 
+
+        foreach ($allSubPlatformsByWorkstream as $work_stream_id => $subPlatforms) {
+            foreach ($subPlatforms as $subPlatform) {
+                $options = new \stdClass();
+                $options->id   = trim($subPlatform);
+                $options->text = trim($subPlatform);
+                $platformWithinStream[$work_stream_id][] = $options;
+            }
+        }
         ?>
         <script>
 		var platformWithinStream = <?= json_encode($platformWithinStream);?>;
 		var workstreamDetails = <?= json_encode($allWorkstreams); ?>;
+
+		console.log(platformWithinStream);
+		console.log(workstreamDetails);
+
+
         </script>
         <?php
 
