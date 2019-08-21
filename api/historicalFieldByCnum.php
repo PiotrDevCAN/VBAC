64740b6a45842ba0500a8f48903fd44ec632d315
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2019-08-21 11:18:06 +0100

    new API historicalFieldByCnum.php

---
+++ api/historicalFieldByCnum.php
@@ -0,0 +1,35 @@
+<?php
+
+use vbac\allTables;
+use vbac\personTable;
+use itdq\DbTable;
+
+ob_start();
+
+if($_REQUEST['token']!= $token){
+    return;
+}
+
+$sql = " SELECT CNUM, " . db2_escape_string($_REQUEST['fieldName']) ;
+$sql.= " FROM " . $_SERVER['environment'] . "." . allTables::$PERSON;
+$sql.= " FOR SYSTEM_TIME as of '" . db2_escape_string($_REQUEST['systemTime']) . "' ";
+$sql.= " ORDER BY CNUM ";
+
+$rs = db2_exec($_SESSION['conn'], $sql);
+$data = array();
+
+if($rs){
+    while(($row = db2_fetch_assoc($rs))==true){
+        $data[] = array_map('trim', $row);
+    }
+} else {
+    ob_clean();
+    DbTable::displayErrorMessage($rs, 'class', 'method', $sql);
+    $errorMessage = ob_get_clean();
+    echo json_encode($errorMessage);
+    exit();
+}
+
+ob_clean();
+$json = json_encode($data);
+echo $json;
