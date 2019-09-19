f41a9ac3c6db55411707a816c64ac8b1ef532356
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2019-09-19 09:53:23 +0100

    Add requestor to the Stop Pes Email

--- ajax/sendPesStopRequestedEmail.php
+++ ajax/sendPesStopRequestedEmail.php
@@ -12,12 +12,13 @@ ob_start();
 AuditTable::audit("Invoked:<b>" . __FILE__ . "</b>Parms:<pre>" . print_r($_POST,true) . "</b>",AuditTable::RECORD_TYPE_AUDIT);
 
 $cnum = $_POST['psm_cnum'];
+$requestor = $_POST['psm_pesrequestor'];
 $pesTracker = new pesTrackerTable(allTables::$PES_TRACKER);
 $response = array();
 $response['success']=false;
 
 try {
-     pesEmail::notifyPesTeamOfOffStopRequest($cnum) ;
+    pesEmail::notifyPesTeamOfOffStopRequest($cnum, $requestor) ;
      $pesTracker->savePesComment($cnum,"PES Stop Requested");
 
      $comment = $pesTracker->getPesComment($cnum);
