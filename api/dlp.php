ec95bc86af199aac3f8c5d2016d0ec3c71ff9d91
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2019-10-04 09:32:26 +0100

    Don't lowercase the cnum

--- api/dlp.php
+++ api/dlp.php
@@ -15,7 +15,7 @@ $status   = !empty($_GET['status'])   ? trim($_GET['status']) : null;
 $sql = " SELECT lower(D.STATUS) as STATUS, count(*) as RECORDS ";
 $sql.= " FROM " . $_SERVER['environment'] . "." . allTables::$DLP . " AS D ";
 $sql.= " WHERE 1=1 AND lower(status) = '" . db2_escape_string($status) . "' " ;
-$sql.= !empty($cnum)     ? " AND D.CNUM = '" . db2_escape_string(strtolower($cnum)) . "'  " : null;
+$sql.= !empty($cnum)     ? " AND D.CNUM = '" . db2_escape_string($cnum) . "'  " : null;
 $sql.= !empty($hostname) ? " AND lower(D.HOSTNAME) = '" . db2_escape_string(strtolower($hostname)) . "'  " : null;
 $sql.= " GROUP BY D.STATUS ";
 
