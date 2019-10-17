edd65bc2a6fa82d8a4da24928ea8de6c79cfe522
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2019-10-17 10:00:29 +0100

    Bluepages API didn't work on my local machine anymore

--- api/bluepages.php
+++ api/bluepages.php
@@ -6,9 +6,15 @@ $url = "https://bluepages.ibm.com/BpHttpApisv3/slaphapi?" . $_SERVER['QUERY_STRI
 
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
 $result = curl_exec($ch);
 
+if(!$result){
+    echo curl_errno($ch);
+    echo curl_error($ch);
+    die('CURL error, see above');
+}
 curl_close($ch);
 
 echo $result;
