4dd620268616aa068cc066d9f081ad26e2177223
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2020-01-14 17:16:41 +0000

    Test emails CC, error with PES reminders

--- cdi_test.php
+++ cdi_test.php
@@ -5,30 +5,47 @@ use vbac\personTable;
 use itdq\Loader;
 use vbac\allTables;
 use vbac\assetRequestRecord;
+use itdq\BlueMail;
 
 // $_SERVER['environment'] = 'VBAC';
 
-$loader = new Loader();
+// $loader = new Loader();
 
-$activePeople = $loader->loadIndexed('REVALIDATION_STATUS','CNUM',allTables::$PERSON,personTable::activePersonPredicate());
+// $activePeople = $loader->loadIndexed('REVALIDATION_STATUS','CNUM',allTables::$PERSON,personTable::activePersonPredicate());
 
-$predicate = " 1=1 ";
-$predicate.= assetRequestRecord::ableToOwnAssets();
-$ableToOwn= $loader->loadIndexed('REVALIDATION_STATUS','CNUM',allTables::$PERSON,$predicate);
+// $predicate = " 1=1 ";
+// $predicate.= assetRequestRecord::ableToOwnAssets();
+// $ableToOwn= $loader->loadIndexed('REVALIDATION_STATUS','CNUM',allTables::$PERSON,$predicate);
 
+// echo "<pre>";
+// var_dump(count($activePeople));
+// echo "</pre>";
+// echo "<pre>";
+// var_dump(count($ableToOwn));
+
+// $difference1 = array_diff_key($ableToOwn, $activePeople);
+// $difference2 = array_diff_key($activePeople, $ableToOwn);
+// echo "</pre>";
+// echo "<pre>";
+
+// echo "<pre>";
+// var_dump($difference1);
+// echo "</pre>";
+// var_dump($difference2);
+// echo "</pre>";
+
+
+$sendResponse = BlueMail::send_mail(array('rob.daniel@uk.ibm.com'), "Test 1",'rob.daniel@uk.ibm.com',array('rob.daniel@uk.ibm.com'));
 echo "<pre>";
-var_dump(count($activePeople));
+print_r($sendResponse);
 echo "</pre>";
-echo "<pre>";
-var_dump(count($ableToOwn));
 
-$difference1 = array_diff_key($ableToOwn, $activePeople);
-$difference2 = array_diff_key($activePeople, $ableToOwn);
-echo "</pre>";
+$sendResponse = BlueMail::send_mail(array('rob.daniel@uk.ibm.com'), "Test 2",'rob.daniel@uk.ibm.com',array());
 echo "<pre>";
+print_r($sendResponse);
+echo "</pre>";
 
+$sendResponse = BlueMail::send_mail(array('rob.daniel@uk.ibm.com'), "Test 3",'rob.daniel@uk.ibm.com',array(""));
 echo "<pre>";
-var_dump($difference1);
-echo "</pre>";
-var_dump($difference2);
+print_r($sendResponse);
 echo "</pre>";
