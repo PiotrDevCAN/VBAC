cf7fc0f79dff7e2b5a867619b0798e3c03caded9
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2020-03-26 17:55:22 +0000

    @channel

--- batchJobs/baudelt_slack_message.php
+++ batchJobs/baudelt_slack_message.php
@@ -3,4 +3,4 @@ use itdq\Trace;
 use itdq\slack;
 
 $slack = new slack();
-$slack->slackPostMessageWithEmoji(slack::CHANNEL_ID_BAU_D_ELT,'*DAILY DELIVERY HEARTBEAT* - Team, please indicate using the emojis below the overall status of your respective areas.', array('thumbsup', 'thumbsdown'));
+$slack->slackPostMessageWithEmoji(slack::CHANNEL_ID_BAU_D_ELT,'<!channel> *DAILY DELIVERY HEARTBEAT* - Team, please indicate using the emojis below the overall status of your respective areas.', array('thumbsup', 'thumbsdown'));
