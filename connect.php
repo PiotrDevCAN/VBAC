0dd04ac011a2a63f57641da6d5bb6949dee11353
Robert Daniel <rob.daniel@uk.ibm.com>
2020-05-19 15:27:39 +0100

    db2

--- connect.php
+++ connect.php
@@ -1,45 +1,54 @@
 <?php
 
-use vbac\personRecord;
-
-if( getenv( "VCAP_SERVICES" ) )
+if( isset($_ENV['ssldsn']) )
 {
     # Get database details from the VCAP_SERVICES environment variable
     #
     # *This can only work if you have used the Bluemix dashboard to
     # create a connection from your dashDB service to your PHP App.
     #
-    $details  = json_decode( getenv( "VCAP_SERVICES" ), true );
-    $dsn      = $details [ "dashDB For Transactions" ][0][ "credentials" ][ "dsn" ];
-    $ssl_dsn  = $details [ "dashDB For Transactions" ][0][ "credentials" ][ "ssldsn" ];
+    //     $details  = json_decode( getenv( "VCAP_SERVICES" ), true );
+    //     $dsn      = $details [ "user-provided" ][0][ "credentials" ][ "dsn" ];
+    //     $ssl_dsn  = $details [ "user-provided" ][0][ "credentials" ][ "ssldsn" ];
+    $ssl_dsn  = $_ENV["ssldsn"]; // pick up from a secret
 
     # Build the connection string
     #
     $driver = "DRIVER={IBM DB2 ODBC DRIVER};";
-    $conn_string = $driver . $dsn;     # Non-SSL
+    //    $conn_string = $driver . $dsn;     # Non-SSL
     $conn_string = $driver . $ssl_dsn; # SSL
 
-    $conn_string = str_replace('poCXUiBIC0Pl','poCXUiBIC0Pl!ab',$conn_string);
-
-    //echo $conn_string;
-
     $conn = db2_connect( $conn_string, "", "" );
-
-//   $_SESSION['ssoEmail'] = 'dummyUser';
     if( $conn )
     {
         $_SESSION['conn'] = $conn;
+        $schema = isset($_SESSION['Db2Schema']) ? $_SESSION['Db2Schema'] : 'REST';
+        $Statement = "SET CURRENT SCHEMA='$schema';";
+        $rs = db2_exec($conn, $Statement);
+
+        if (! $rs) {
+            echo "<br/>" . $Statement . "<br/>";
+            echo "<BR>" . db2_stmt_errormsg() . "<BR>";
+            echo "<BR>" . db2_stmt_error() . "<BR>";
+            exit("Set current schema failed");
+        }
         db2_autocommit($conn, TRUE); // This is how it was on the Wintel Box - so the code has no/few commit points.
     }
     else
     {
+        error_log(__FILE__ . __LINE__ . " Connect to DB2 Failed");
+        error_log(__FILE__ . __LINE__ . $conn_string);
+        error_log(__FILE__ . __LINE__ . db2_conn_errormsg());
+        error_log(__FILE__ . __LINE__ . db2_conn_error());
+
         echo "<p>Connection failed.</p>";
-        echo db2_conn_error();
-        echo db2_conn_errormsg();
     }
 }
 else
 {
+    echo "<pre>";
+    print_r($_ENV);
+    echo "</pre>";
     echo "<p>No credentials.</p>";
 }
 
