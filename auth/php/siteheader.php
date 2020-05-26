61fca8268478d42c2dff2c1ffff15268b5d65ebc
Robert Daniel <rob.daniel@uk.ibm.com>
2020-05-26 10:56:54 +0100

    Little bit of JWT

--- auth/php/siteheader.php
+++ auth/php/siteheader.php
@@ -1,4 +1,16 @@
 <?php
+
+use ByJG\Session\JwtSession;
+
+include ('vendor/autoload.php');
+include ('splClassLoader.php');
+
+$sessionConfig = (new \ByJG\Session\SessionConfig($_SERVER['SERVER_NAME']))
+->withSecret($_ENV['jwt_token']);
+
+$handler = new \ByJG\Session\JwtSession($sessionConfig);
+session_set_save_handler($handler, true);
+
 error_log("Back from SSO");
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -7,6 +19,6 @@ date_default_timezone_set('UTC');
 
 set_include_path("./" . PATH_SEPARATOR . "../" . PATH_SEPARATOR . "../../" . PATH_SEPARATOR . "../../../" . PATH_SEPARATOR);
 
-include "vendor/autoload.php";
-include "splClassLoader.php";
+// include "vendor/autoload.php";
+// include "splClassLoader.php";
 include "connect.php";
