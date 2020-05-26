273ae9c41414656f91ed3edf8e7fa93b7d80c858
Robert Daniel <rob.daniel@uk.ibm.com>
2020-05-26 11:05:08 +0100

    fix autoload

--- auth/php/siteheader.php
+++ auth/php/siteheader.php
@@ -2,6 +2,14 @@
 
 use ByJG\Session\JwtSession;
 
+error_log("Back from SSO");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+date_default_timezone_set('UTC');
+
+set_include_path("./" . PATH_SEPARATOR . "../" . PATH_SEPARATOR . "../../" . PATH_SEPARATOR . "../../../" . PATH_SEPARATOR);
+
 include ('vendor/autoload.php');
 include ('splClassLoader.php');
 
@@ -11,14 +19,5 @@ $sessionConfig = (new \ByJG\Session\SessionConfig($_SERVER['SERVER_NAME']))
 $handler = new \ByJG\Session\JwtSession($sessionConfig);
 session_set_save_handler($handler, true);
 
-error_log("Back from SSO");
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-date_default_timezone_set('UTC');
-
-set_include_path("./" . PATH_SEPARATOR . "../" . PATH_SEPARATOR . "../../" . PATH_SEPARATOR . "../../../" . PATH_SEPARATOR);
-
-// include "vendor/autoload.php";
-// include "splClassLoader.php";
+session_start();
 include "connect.php";
