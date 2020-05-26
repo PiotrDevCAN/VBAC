c2cc9f18b6fbf3f6d83a9ba1894ad47d9b62db66
Robert Daniel <rob.daniel@uk.ibm.com>
2020-05-26 12:34:07 +0100

    Don't call Ob_clean if no buffers exist

--- ajax/createHtmlForPersonTable.php
+++ ajax/createHtmlForPersonTable.php
@@ -3,8 +3,8 @@ use vbac\allTables;use vbac\personRecord;
 set_time_limit(0);
 ob_start();
 $personRecord = new personRecord();
-$headerCells = $personRecord->htmlHeaderCells();
-ob_clean();?>
+$headerCells = $personRecord->htmlHeaderCells();while(ob_get_level()>1){
+    ob_clean();}?>
 <table id='personTable' class='table table-striped table-bordered compact'   style='width:100%'>
 <thead>
 <tr><?=$headerCells;?></tr></thead>
