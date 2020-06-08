1a7ebeb7e61b05b0e99a501d038498a5057ec07d
Robert Daniel <rob.daniel@uk.ibm.com>
2020-06-08 12:49:18 +0100

    Need to setpath before includes

--- batchJobs/php/siteheader.php
+++ batchJobs/php/siteheader.php
@@ -1,6 +1,14 @@
 <?php
 //use ByJG\Session\JwtSession;
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+date_default_timezone_set('UTC');
+
+set_include_path("./" . PATH_SEPARATOR . "../" . PATH_SEPARATOR . "../../" . PATH_SEPARATOR . "../../../" . PATH_SEPARATOR);
+session_start();
+
 include ('vendor/autoload.php');
 include ('splClassLoader.php');
 
@@ -11,12 +19,12 @@ include ('splClassLoader.php');
 // session_set_save_handler($handler, true);
 
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-date_default_timezone_set('UTC');
-set_include_path("./" . PATH_SEPARATOR . "../" . PATH_SEPARATOR . "../../" . PATH_SEPARATOR . "../../../" . PATH_SEPARATOR);
-session_start();
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+// date_default_timezone_set('UTC');
+// set_include_path("./" . PATH_SEPARATOR . "../" . PATH_SEPARATOR . "../../" . PATH_SEPARATOR . "../../../" . PATH_SEPARATOR);
+// session_start();
 
 include "../php/w3config.php";
 include "connect.php";
