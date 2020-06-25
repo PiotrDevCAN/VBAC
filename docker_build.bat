9f247accc7eb2bdc1397f40d8bec5c3e5211cb11
Robert Daniel <rob.daniel@uk.ibm.com>
2020-06-25 12:20:30 +0100

    name the image locally

--- docker_build.bat
+++ docker_build.bat
@@ -1,2 +1,2 @@
 docker build -t vbac . --no-cache 
-docker run -dit -p 8082:8080 -v C:/Users/RobDaniel/git/vBAC_bm:/var/www/html --env-file C:/Users/RobDaniel/git/vBAC_bm/dev_env.list vbac
+docker run -dit -p 8082:8080  --name vbac -v C:/Users/RobDaniel/git/vBAC_bm:/var/www/html --env-file C:/Users/RobDaniel/git/vBAC_bm/dev_env.list vbac
