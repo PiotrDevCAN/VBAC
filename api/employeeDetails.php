f86fc65caad4ced224baf6931be834de6bbf3b9d
Robert Daniel <rob.daniel@uk.ibm.com>
2020-07-02 11:39:55 +0100

    Add Squad Details to what this API returns.

--- api/employeeDetails.php
+++ api/employeeDetails.php
@@ -8,8 +8,6 @@ if($_REQUEST['token']!= $token){
     return;
 }
 
-
-
 $emailID = !empty($_GET['emailid']) ? $_GET['emailid'] : null;
 $notesId = !empty($_GET['notesid']) ? $_GET['notesid'] : null;
 $cnum    = !empty($_GET['cnum']) ? $_GET['cnum'] : null;
@@ -18,15 +16,28 @@ $trimmedRow = false;
 
 $sql = " SELECT P.*, M.EMAIL_ADDRESS as FM_EMAIL, M.NOTES_ID as FM_NOTES_ID,  trim(P.FIRST_NAME) CONCAT ' ' CONCAT trim(P.LAST_NAME) as FULL_NAME  ";
 $sql.= " , CASE WHEN T.DESCRIPTION is not null then T.DESCRIPTION else P.EMPLOYEE_TYPE end as EMPLOYEE_TYPE ";
+$sql.= " , SQ.SQUAD_NAME, SQ.SQUAD_LEADER, SQP.EMAIL_ADDRESS as SQUAD_LEADER_EMAIL ";
+$sql.= " , TR.TRIBE_NAME, TR.TRIBE_LEADER, TRP.EMAIL_ADDRESS as TRIBE_LEADER_EMAIL ";
+$sql.= " , 'tbc' as ITERATION_MGR ";
 $sql.= " FROM " . $_ENV['environment'] . "." . allTables::$PERSON . " AS P ";
 $sql.= " LEFT JOIN " . $_ENV['environment'] . "." . allTables::$PERSON . " AS M ";
 $sql.= " ON P.FM_CNUM = M.CNUM ";
 $sql.= " LEFT JOIN " . $_ENV['environment'] . "." . allTables::$EMPLOYEE_TYPE_MAPPING .  " AS T ";
 $sql.= " ON upper(P.EMPLOYEE_TYPE) = upper(T.CODE) ";
+$sql.= " LEFT JOIN " . $_ENV['environment'] . "." . allTables::$AGILE_SQUAD .  " AS SQ ";
+$sql.= " ON P.SQUAD_NUMBER = SQ.SQUAD_NUMBER ";
+$sql.= " LEFT JOIN " . $_ENV['environment'] . "." . allTables::$AGILE_TRIBE .  " AS TR ";
+$sql.= " ON SQ.TRIBE_NUMBER = SQ.TRIBE_NUMBER ";
+$sql.= " LEFT JOIN " . $_ENV['environment'] . "." . allTables::$PERSON .  " AS SQP ";
+$sql.= " ON SQ.SQUAD_LEADER = SQP.NOTES_ID ";
+$sql.= " LEFT JOIN " . $_ENV['environment'] . "." . allTables::$PERSON .  " AS TRP ";
+$sql.= " ON TR.TRIBE_LEADER = TRP.NOTES_ID ";
 $sql.= " WHERE 1=1 ";
 $sql.= !empty($emailID) ? " AND lower(P.EMAIL_ADDRESS) = '" . db2_escape_string(strtolower($emailID)) . "'; " : null;
 $sql.= !empty($notesId) ? " AND lower(P.NOTES_ID) = '" . db2_escape_string(strtolower($notesId)) . "'; " : null;
 $sql.= !empty($cnum) ? " AND lower(P.CNUM) = '" . db2_escape_string(strtolower($cnum)) . "'; " : null;
+
+error_log($sql);
 
 $rs = db2_exec($_SESSION['conn'], $sql);
 
