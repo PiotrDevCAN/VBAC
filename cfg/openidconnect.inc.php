07d78c66a35dff57d562d0f012210a4d07721052
Robert Daniel <rob.daniel@uk.ibm.com>
2020-08-27 10:44:54 +0100

    Error_log the Authorize URL and Client ID for debug.

--- cfg/openidconnect.inc.php
+++ cfg/openidconnect.inc.php
@@ -24,11 +24,13 @@ $config_openidconnect = new stdClass();
  */
 
 $config_openidconnect->client_id      = $_ENV['sso_client_id'];
-$config_openidconnect->client_secret  = $_ENV['sso_client_secret'];;
+$config_openidconnect->client_secret  = $_ENV['sso_client_secret'];
 
 $config_openidconnect->authorize_url  = $_ENV['sso_authorize_url'];
-$config_openidconnect->token_url      = $_ENV['sso_token_url'];;
-$config_openidconnect->introspect_url = $_ENV['sso_introspect_url'];;
+$config_openidconnect->token_url      = $_ENV['sso_token_url'];
+$config_openidconnect->introspect_url = $_ENV['sso_introspect_url'];
+
+error_log('Authorising to:' . $config_openidconnect->authorize_url . " as (" . $config_openidconnect->client_id . ") ");
 
 
 
