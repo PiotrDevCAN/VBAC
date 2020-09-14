3ea882a860f02fe263ef7a1e1a010f15af8b23c1
Robert Daniel <rob.daniel@uk.ibm.com>
2020-09-14 12:48:54 +0100

    Test

--- batchJobs/recheckPotentialLeavers.php
+++ batchJobs/recheckPotentialLeavers.php
@@ -14,51 +14,51 @@ $slack = new slack();
 AuditTable::audit("Revalidation recheck potential leavers invoked.",AuditTable::RECORD_TYPE_REVALIDATION);
 $slack->sendMessageToChannel("Revalidation recheck potential leavers invoked.", slack::CHANNEL_SM_CDI_AUDIT);
 
-set_time_limit(60);
+// set_time_limit(60);
 
-$personTable = new personTable(allTables::$PERSON);
-$slack = new slack();
-$loader = new Loader();
+// $personTable = new personTable(allTables::$PERSON);
+// $slack = new slack();
+// $loader = new Loader();
 
-$potentialLeavers = " ( REVALIDATION_STATUS like  'potential%') ";
-$allpotentialLeavers = $loader->load('CNUM',allTables::$PERSON, $potentialLeavers ); //
-AuditTable::audit("Revalidation re-check will re-check " . count($allpotentialLeavers) . " potential leavers.",AuditTable::RECORD_TYPE_REVALIDATION);
-$response = $slack->slackApiPostMessage(slack::CHANNEL_ID_SM_CDI_AUDIT,$_ENV['environment'] . ":Revalidation re-check will re-check " . count($allpotentialLeavers) . " potential leavers.", slack::CHANNEL_SM_CDI_AUDIT);
-error_log($response);
-$chunkedCnum = array_chunk($allpotentialLeavers, 400);
-$detailsFromBp = "&notesid&mail";
-$bpEntries = array();
+// $potentialLeavers = " ( REVALIDATION_STATUS like  'potential%') ";
+// $allpotentialLeavers = $loader->load('CNUM',allTables::$PERSON, $potentialLeavers ); //
+// AuditTable::audit("Revalidation re-check will re-check " . count($allpotentialLeavers) . " potential leavers.",AuditTable::RECORD_TYPE_REVALIDATION);
+// $response = $slack->slackApiPostMessage(slack::CHANNEL_ID_SM_CDI_AUDIT,$_ENV['environment'] . ":Revalidation re-check will re-check " . count($allpotentialLeavers) . " potential leavers.", slack::CHANNEL_SM_CDI_AUDIT);
+// error_log($response);
+// $chunkedCnum = array_chunk($allpotentialLeavers, 400);
+// $detailsFromBp = "&notesid&mail";
+// $bpEntries = array();
 
-foreach ($chunkedCnum as $key => $cnumList){
-    $bpEntries[$key] = BluePages::getDetailsFromCnumSlapMulti($cnumList, $detailsFromBp);
-    foreach ($bpEntries[$key]->search->entry as $bpEntry){
-        set_time_limit(20);
-        $serial = substr($bpEntry->dn,4,9);
-        $mail        = ''; // Clear out previous value
-        $notesid     = ''; // Clear out previous value
-        foreach ($bpEntry->attribute as $details){
-            $name = trim($details->name);
-            $$name = trim($details->value[0]);
-        }
-        $notesid = str_replace(array('CN=','OU=','O='),array('','',''),$notesid);
-        $personTable->confirmRevalidation($notesid,$mail,$serial);
-        unset($allpotentialLeavers[$serial]);
-    }
-}
+// foreach ($chunkedCnum as $key => $cnumList){
+//     $bpEntries[$key] = BluePages::getDetailsFromCnumSlapMulti($cnumList, $detailsFromBp);
+//     foreach ($bpEntries[$key]->search->entry as $bpEntry){
+//         set_time_limit(20);
+//         $serial = substr($bpEntry->dn,4,9);
+//         $mail        = ''; // Clear out previous value
+//         $notesid     = ''; // Clear out previous value
+//         foreach ($bpEntry->attribute as $details){
+//             $name = trim($details->name);
+//             $$name = trim($details->value[0]);
+//         }
+//         $notesid = str_replace(array('CN=','OU=','O='),array('','',''),$notesid);
+//         $personTable->confirmRevalidation($notesid,$mail,$serial);
+//         unset($allpotentialLeavers[$serial]);
+//     }
+// }
 
-// At this stage, anyone still in the $allNonLeavers array - has NOT been found in BP TWICE and so is now a leaver and needs to be flagged as such.
-AuditTable::audit("Revalidation re-check found " . count($allpotentialLeavers) . "  leavers.",AuditTable::RECORD_TYPE_REVALIDATION);
-$response = $slack->slackApiPostMessage(slack::CHANNEL_ID_SM_CDI_AUDIT,$_ENV['environment'] . ":Revalidation re-check found " . count($allpotentialLeavers) . "  leavers.", slack::CHANNEL_SM_CDI_AUDIT);
-error_log($response);
+// // At this stage, anyone still in the $allNonLeavers array - has NOT been found in BP TWICE and so is now a leaver and needs to be flagged as such.
+// AuditTable::audit("Revalidation re-check found " . count($allpotentialLeavers) . "  leavers.",AuditTable::RECORD_TYPE_REVALIDATION);
+// $response = $slack->slackApiPostMessage(slack::CHANNEL_ID_SM_CDI_AUDIT,$_ENV['environment'] . ":Revalidation re-check found " . count($allpotentialLeavers) . "  leavers.", slack::CHANNEL_SM_CDI_AUDIT);
+// error_log($response);
 
-foreach ($allpotentialLeavers as $cnum){
-    set_time_limit(10);
-    $personTable->flagLeaver($cnum);
-}
+// foreach ($allpotentialLeavers as $cnum){
+//     set_time_limit(10);
+//     $personTable->flagLeaver($cnum);
+// }
 
-pesEmail::notifyPesTeamOfLeavers($allpotentialLeavers);
+// pesEmail::notifyPesTeamOfLeavers($allpotentialLeavers);
 
-AuditTable::audit("Revalidation re-check completed.",AuditTable::RECORD_TYPE_REVALIDATION);
-$response = $slack->slackApiPostMessage(slack::CHANNEL_ID_SM_CDI_AUDIT,$_ENV['environment'] . ":Revalidation re-check completed.", slack::CHANNEL_SM_CDI_AUDIT);
-error_log($response);
-db2_commit($GLOBALS['conn']);
+// AuditTable::audit("Revalidation re-check completed.",AuditTable::RECORD_TYPE_REVALIDATION);
+// $response = $slack->slackApiPostMessage(slack::CHANNEL_ID_SM_CDI_AUDIT,$_ENV['environment'] . ":Revalidation re-check completed.", slack::CHANNEL_SM_CDI_AUDIT);
+// error_log($response);
+// db2_commit($GLOBALS['conn']);
