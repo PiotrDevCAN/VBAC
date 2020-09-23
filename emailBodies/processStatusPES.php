76815d3a0c50d1cf6946c4ce5ff71eac4784eaf5
Robert Daniel <rob.daniel@uk.ibm.com>
2020-09-23 13:35:51 +0100

    PES team need 10 days not 7 to respond.

--- emailBodies/processStatusPES.php
+++ emailBodies/processStatusPES.php
@@ -1,6 +1,6 @@
 <?php
 $pesEmail = 'Hello &&firstName&&,';
-$pesEmail.= '<p>I confirm that the PES team have received your documents/information and we are working on your case. We will get back to you within 7 days with an update.</p>';
+$pesEmail.= '<p>I confirm that the PES team have received your documents/information and we are working on your case. We will get back to you within 10 days with an update.</p>';
 $pesEmail.= '<p>Many Thanks for your cooperation,</p>';
 $pesEmail.= '<h3>Lloyds PES Team</h3>';
 
