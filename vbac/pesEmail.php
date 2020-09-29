d21f075a1701091a156695968310d546fc6a6386
Robert Daniel <rob.daniel@uk.ibm.com>
2020-09-29 10:54:23 +0100

    Owens_Consent_Form.pdf

--- vbac/pesEmail.php
+++ vbac/pesEmail.php
@@ -110,7 +110,7 @@ class pesEmail {
             case 'recheck_L1_India_Non_Core4.php':
                 $pesAttachments = array('../emailAttachments/Lloyds Global Application Form v2.0.doc'
                                        ,'../emailAttachments/ODC Global Application Form v2.0.doc'
-                                       ,'../emailAttachments/Ownes_Consent_Form.doc');
+                                       ,'../emailAttachments/Owens_Consent_Form.pdf');
                 break;
             case 'recheck_L1_UK.php':
                 $pesAttachments = array('../emailAttachments/Lloyds Global Application Form v2.0.doc');
@@ -121,7 +121,7 @@ class pesEmail {
             case 'recheck_L2_India_Non_Core4.php':
                 $pesAttachments = array('../emailAttachments/Lloyds Global Application Form v2.0.doc'
                                        ,'../emailAttachments/ODC Global Application Form v2.0.doc'
-                                       ,'../emailAttachments/Ownes_Consent_Form.doc');
+                                       ,'../emailAttachments/Owens_Consent_Form.pdf');
                 break;
             case 'recheck_L2_UK.php':
                 $pesAttachments = array('../emailAttachments/Lloyds Global Application Form v2.0.doc');
