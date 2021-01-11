007dbcde9a3fc51a11942c88d4185ad6600db082
Robert Daniel <rob.daniel@uk.ibm.com>
2021-01-11 11:32:32 +0000

    unnecessary ob_clean();

--- ajax/createHtmlForPersonFinderTable.php
+++ ajax/createHtmlForPersonFinderTable.php
@@ -1,6 +1,3 @@
-<?php
-ob_clean();
-?>
 <table id='personFinderTable' class='table table-striped table-bordered compact'   style='width:100%'>
 <thead>
 <tr><th>Cnum</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Notes Id</th><th>FM</th></tr></thead>
