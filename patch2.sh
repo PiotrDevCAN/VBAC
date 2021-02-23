39c87cad5258ac0ad5879b3aaf3b65ad0493cb88
Robert Daniel <rob.daniel@uk.ibm.com>
2021-02-23 11:49:20 +0000

    Don't overwrite index.php

--- patch2.sh
+++ patch2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "<?php phpinfo();?>" > /var/www/html/index.php
+#echo "<?php phpinfo();?>" > /var/www/html/index.php
 echo 'auto_prepend_file="php/siteheader.php"' > /etc/php.d/rob.ini
 echo 'auto_append_file="php/sitefooter.php"' >> /etc/php.d/rob.ini
 echo 'memory_limit=512M' >> /etc/php.d/rob.ini
