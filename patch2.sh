5838baa023bbee2f8198ab4ef896f38c3d41e20d
Robert Daniel <rob.daniel@uk.ibm.com>
2021-02-23 10:51:28 +0000

    Use the Siteheader relative to the module being invoked.

--- patch2.sh
+++ patch2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "<?php phpinfo();?>" > /var/www/html/index.php
-echo 'auto_prepend_file="/var/www/html/php/siteheader.php"' > /etc/php.d/rob.ini
-echo 'auto_append_file="/var/www/html/php/sitefooter.php"' >> /etc/php.d/rob.ini
+echo 'auto_prepend_file="php/siteheader.php"' > /etc/php.d/rob.ini
+echo 'auto_append_file="php/sitefooter.php"' >> /etc/php.d/rob.ini
 echo 'memory_limit=512M' >> /etc/php.d/rob.ini
