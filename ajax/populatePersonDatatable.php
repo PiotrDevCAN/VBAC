ceaa9aba92ab5a5872239eec27d812cdf42cb598
Piotr Tajanowicz <piotr.tajanowicz@ibm.com>
2021-03-19 15:28:59 +0100

    optimization work

--- ajax/populatePersonDatatable.php
+++ ajax/populatePersonDatatable.php
@@ -3,10 +3,15 @@
 use vbac\personTable;
 use vbac\allTables;
 
+
 set_time_limit(0);
 ob_start();
 
 // session_start();
+
+function ob_html_compress($buf){
+    return str_replace(array("\n","\r"),'',$buf);
+}
 
 $personTable = new personTable(allTables::$PERSON);
 $preBoardersAction = isset($_REQUEST['preBoardersAction']) ? $_REQUEST['preBoardersAction'] : null;
@@ -19,13 +24,25 @@ $messages = ob_get_clean();
 ob_start();
 
 if($dataJsonAble) {
-     $response = array("data"=>$data,'messages'=>$messages,'post'=>print_r($_POST,true));
- } else {
+    $response = array("data"=>$data,'messages'=>$messages,'post'=>print_r($_POST,true));
+} else {
     $personTable->findDirtyData();
     $dirtyDetails = ob_get_clean();
     ob_start();
     echo $dirtyDetails;
     exit();
- }
+}
 ob_clean();
+
+if (isset($_SERVER['HTTP_ACCEPT_ENCODING'])) {
+    if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
+        ob_start("ob_gzhandler");
+    } else {
+        ob_start("ob_html_compress");
+    }
+} else {
+    ob_start("ob_html_compress");
+}
+
 echo json_encode($response);
+
--- ajax/populatePersonPortalLite.php
+++ ajax/populatePersonPortalLite.php
@@ -9,6 +9,10 @@ ob_start();
 
 // session_start();
 
+function ob_html_compress($buf){
+   return str_replace(array("\n","\r"),'',$buf);
+}
+
 $personTable = new personPortalLiteTable(allTables::$PERSON_PORTAL_LITE);
 $preBoardersAction = isset($_REQUEST['preBoardersAction']) ? $_REQUEST['preBoardersAction'] : null;
 
@@ -20,13 +24,24 @@ $messages = ob_get_clean();
 ob_start();
 
 if($dataJsonAble) {
-     $response = array("data"=>$data,'messages'=>$messages,'post'=>print_r($_POST,true));
- } else {
-    $personTable->findDirtyData();
-    $dirtyDetails = ob_get_clean();
-    ob_start();
-    echo $dirtyDetails;
-    exit();
- }
+   $response = array("data"=>$data,'messages'=>$messages,'post'=>print_r($_POST,true));
+} else {
+   $personTable->findDirtyData();
+   $dirtyDetails = ob_get_clean();
+   ob_start();
+   echo $dirtyDetails;
+   exit();
+}
 ob_clean();
+
+if (isset($_SERVER['HTTP_ACCEPT_ENCODING'])) {
+   if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
+       ob_start("ob_gzhandler");
+   } else {
+       ob_start("ob_html_compress");
+   }
+} else {
+   ob_start("ob_html_compress");
+}
+
 echo json_encode($response);
