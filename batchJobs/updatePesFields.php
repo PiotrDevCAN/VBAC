ff034894f9c2d19e0608415e8ee4e2aa5c2c5dcf
Piotr <piotr.tajanowicz@ibm.com>
2021-09-10 13:36:04 +0200

    script optimisation

--- batchJobs/updatePesFields.php
+++ batchJobs/updatePesFields.php
@@ -36,78 +36,84 @@ if (isset($url)) {
     $commitEvery100Updates = 100;
 
     $loader = new Loader();
-    $employeesEmailsForRaw = $loader->load('EMAIL_ADDRESS', allTables::$PERSON);
-    $employeesEmailsFor = array_map('strtolower',$employeesEmailsForRaw);
+    $employeesEmailsRaw = $loader->load('EMAIL_ADDRESS', allTables::$PERSON);
+    $employeesEmails = array_map('strtolower',$employeesEmailsRaw);
 
-    var_dump($employeesEmailsFor);
-    exit;
+    // var_dump($employeesEmails);
+    // exit;
 
     foreach ($pesDataAll->data as $upesData){
-        // create and prepare the update statment to the PERSON table
+        if (in_array(strtolower(trim($upesData->EMAIL_ADDRESS)), $employeesEmails)) {
+            // create and prepare the update statment to the PERSON table
         
-        $updatePersonSql = " UPDATE " . $GLOBALS['Db2Schema'] . "." . allTables::$PERSON ;
-        $updatePersonSql.= " SET ";
-        $personData = array();
-        
-        foreach ($personFields as $fieldName) {
-            $$fieldName = !empty($upesData->$fieldName) ? $upesData->$fieldName : null;
-            $updatePersonSql.= !empty($upesData->$fieldName) ? $fieldName . "=?, " : null;
-            !empty($upesData->$fieldName) ? $personData[] = $$fieldName : null;        
-        }
-        
-        $updatePersonSql = substr($updatePersonSql,0,-2) . " WHERE lower(EMAIL_ADDRESS)=? ";
-        $personData[] = strtolower($upesData->EMAIL_ADDRESS) ;
-        
-        $preparedUpdatePersonSql = db2_prepare($GLOBALS['conn'], $updatePersonSql);
-        
-        if(!$preparedUpdatePersonSql){
-            echo db2_stmt_error();
-            echo db2_stmt_errormsg();
-            print_r($personData);
-            DbTable::displayErrorMessage($preparedUpdatePersonSql, __FILE__, __FILE__, $updatePersonSql);
-            return;
-        }
+            $updatePersonSql = " UPDATE " . $GLOBALS['Db2Schema'] . "." . allTables::$PERSON ;
+            $updatePersonSql.= " SET ";
+            $personData = array();
             
-        // create and prepare the update statment to the PES_TRACKER table
+            foreach ($personFields as $fieldName) {
+                $$fieldName = !empty($upesData->$fieldName) ? $upesData->$fieldName : null;
+                $updatePersonSql.= !empty($upesData->$fieldName) ? $fieldName . "=?, " : null;
+                !empty($upesData->$fieldName) ? $personData[] = $$fieldName : null;        
+            }
+            
+            $updatePersonSql = substr($updatePersonSql,0,-2) . " WHERE lower(EMAIL_ADDRESS)=? ";
+            $personData[] = strtolower($upesData->EMAIL_ADDRESS) ;
+            
+            $preparedUpdatePersonSql = db2_prepare($GLOBALS['conn'], $updatePersonSql);
+            
+            if(!$preparedUpdatePersonSql){
+                echo db2_stmt_error();
+                echo db2_stmt_errormsg();
+                print_r($personData);
+                DbTable::displayErrorMessage($preparedUpdatePersonSql, __FILE__, __FILE__, $updatePersonSql);
+                return;
+            }
+                
+            // create and prepare the update statment to the PES_TRACKER table
+            
+            $updatePesTrackerSql = " UPDATE " . $GLOBALS['Db2Schema'] . "." . allTables::$PES_TRACKER ;
+            $updatePesTrackerSql.= " SET ";
+            $pesTrackerData = array();
+            
+            foreach ($pesTrackerFields as $fieldName) {
+                $$fieldName = !empty($upesData->$fieldName) ? $upesData->$fieldName : null;
+                $updatePesTrackerSql.= !empty($upesData->$fieldName) ? $fieldName . "=?, " : null;
+                !empty($upesData->$fieldName) ? $pesTrackerData[] = $$fieldName : null;
+            }
         
-        $updatePesTrackerSql = " UPDATE " . $GLOBALS['Db2Schema'] . "." . allTables::$PES_TRACKER ;
-        $updatePesTrackerSql.= " SET ";
-        $pesTrackerData = array();
-        
-        foreach ($pesTrackerFields as $fieldName) {
-            $$fieldName = !empty($upesData->$fieldName) ? $upesData->$fieldName : null;
-            $updatePesTrackerSql.= !empty($upesData->$fieldName) ? $fieldName . "=?, " : null;
-            !empty($upesData->$fieldName) ? $pesTrackerData[] = $$fieldName : null;
+            $updatePesTrackerSql = substr($updatePesTrackerSql,0,-2) . " WHERE CNUM=( SELECT CNUM FROM " . $GLOBALS['Db2Schema'] . "." . allTables::$PERSON . " WHERE lower(EMAIL_ADDRESS) = ? FETCH FIRST 1 ROWS ONLY ) ";
+            $pesTrackerData[]    = strtolower($upesData->EMAIL_ADDRESS) ;
+            
+            $preparedUpdatePesTrackerSql = db2_prepare($GLOBALS['conn'], $updatePesTrackerSql);
+            
+            if(!$preparedUpdatePesTrackerSql){
+                echo db2_stmt_error();
+                echo db2_stmt_errormsg();
+                print_r($pesTrackerData);
+                DbTable::displayErrorMessage($preparedUpdatePesTrackerSql, __FILE__, __FILE__, $updatePesTrackerSql);
+                return;
+            }
+            
+            $rsPerson = db2_execute($preparedUpdatePersonSql,$personData);    
+            if(!$rsPerson){
+                DbTable::displayErrorMessage($rsPerson, __FILE__, __FILE__, $updatePersonSql);     
+                db2_rollback($GLOBALS['conn']);
+            }
+            
+            $rsPesTracker = db2_execute($preparedUpdatePesTrackerSql,$pesTrackerData);
+            if(!$rsPesTracker){
+                DbTable::displayErrorMessage($rsPesTracker, __FILE__, __FILE__, $updatePesTrackerSql);
+                db2_rollback($GLOBALS['conn']);
+            }
+            
+            echo "<br/> Updated PES_TRACKER and PERSON Records for Email:" . $upesData->EMAIL_ADDRESS;
+            
+            db2_commit($GLOBALS['conn']);
+        } else {
+            
+            echo "<br/> Email:" . $upesData->EMAIL_ADDRESS . " read from PES not found in vBAC.";
+            
         }
-    
-        $updatePesTrackerSql = substr($updatePesTrackerSql,0,-2) . " WHERE CNUM=( SELECT CNUM FROM " . $GLOBALS['Db2Schema'] . "." . allTables::$PERSON . " WHERE lower(EMAIL_ADDRESS) = ? FETCH FIRST 1 ROWS ONLY ) ";
-        $pesTrackerData[]    = strtolower($upesData->EMAIL_ADDRESS) ;
-        
-        $preparedUpdatePesTrackerSql = db2_prepare($GLOBALS['conn'], $updatePesTrackerSql);
-        
-        if(!$preparedUpdatePesTrackerSql){
-            echo db2_stmt_error();
-            echo db2_stmt_errormsg();
-            print_r($pesTrackerData);
-            DbTable::displayErrorMessage($preparedUpdatePesTrackerSql, __FILE__, __FILE__, $updatePesTrackerSql);
-            return;
-        }
-        
-        $rsPerson = db2_execute($preparedUpdatePersonSql,$personData);    
-        if(!$rsPerson){
-            DbTable::displayErrorMessage($rsPerson, __FILE__, __FILE__, $updatePersonSql);     
-            db2_rollback($GLOBALS['conn']);
-        }
-        
-        $rsPesTracker = db2_execute($preparedUpdatePesTrackerSql,$pesTrackerData);
-        if(!$rsPesTracker){
-            DbTable::displayErrorMessage($rsPesTracker, __FILE__, __FILE__, $updatePesTrackerSql);
-            db2_rollback($GLOBALS['conn']);
-        }
-        
-        echo "<br/> Updated PES_TRACKER and PERSON Records for Email:" . $upesData->EMAIL_ADDRESS;
-        
-        db2_commit($GLOBALS['conn']);
     }
 } else {
     throw new \Exception('PES Application URL was missing.');
