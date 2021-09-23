058d78b8ce307df6a14819bc6aef487fd084a81e
Piotr <piotr.tajanowicz@ibm.com>
2021-09-23 12:29:10 +0200

    onboarding fix

--- vbac/java/personRecord.js
+++ vbac/java/personRecord.js
@@ -95,7 +95,7 @@ function personRecord() {
       } else {
         // no need to check
         $('#resource_email').val('');
-        $('#resource_email').css("background-color","white").attr('placeholder','Email Address');	
+        $('#resource_email').css("background-color","white");
       }
 	  });
 
