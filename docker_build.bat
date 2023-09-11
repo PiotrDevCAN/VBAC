9eeefb6ad8f01f79840dfd67e25252e0ec5842de
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-09-11 18:37:04 +0200

    Record save fix 3

--- docker_build.bat
+++ docker_build.bat
@@ -1,2 +1,2 @@
-docker build -t vbac . --no-cache 
+docker build -t vbac --no-cache  --progress=plain  . 2> build.log
 docker run -dit -p 8082:8080  --name vbac -v C:/CETAapps/VBAC:/var/www/html --env-file C:/CETAapps/VBAC/dev_env.list vbac
