708bcf281f85454ffb29722a71416cab4174e355
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-11-13 15:09:11 +0100

    Okta Groups table

--- vbac/java/cdi/OKTAUserList.js
+++ vbac/java/cdi/OKTAUserList.js
@@ -35,14 +35,14 @@ class OKTAUserList {
                 "data": {
                     "group": groupName
                 },
-                beforeSend: function (jqXHR, settings) {
-                    $.each(xhrPool, function (idx, jqXHR) {
-                        console.log('abort jqXHR');
-                        jqXHR.abort();  // basically, cancel any existing request, so this one is the only one running
-                        xhrPool.splice(idx, 1);
-                    });
-                    xhrPool.push(jqXHR);
-                }
+                // beforeSend: function (jqXHR, settings) {
+                //     $.each(xhrPool, function (idx, jqXHR) {
+                //         console.log('abort jqXHR');
+                //         jqXHR.abort();  // basically, cancel any existing request, so this one is the only one running
+                //         xhrPool.splice(idx, 1);
+                //     });
+                //     xhrPool.push(jqXHR);
+                // }
             },
             columns: [
                 { data: "NAME", "defaultContent": "" },
