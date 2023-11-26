47b810c783e5483f23f240e9263aa57dbde5f9b1
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-11-26 15:37:47 +0100

    Background Process - nohup file

--- vbac/batchJob.php
+++ vbac/batchJob.php
@@ -34,10 +34,17 @@ class batchJob
     function run() {
         try {
             // $cmd = 'nohup '.$this->cmd.' > /dev/null 2>&1 & echo $!';
-            $cmd = 'nohup '.$this->cmd.' > nohup.out & > /dev/null';
+            // $cmd = 'nohup '.$this->cmd.' > nohup.out & > /dev/null';
 
-            $process = new BackgroundProcess($cmd);
-            $process->run();
+            $process = new BackgroundProcess($this->cmd);
+            $process->run('nohup.out');
+
+            sleep(5);
+            if ($process->isRunning()) {
+                echo 'running';
+            } else {
+                echo 'stopped';
+            }
 
             echo $this->processName." has succeed to be executed: ".$this->email.PHP_EOL;
             error_log($this->processName." has succeed to be executed: ".$this->email.PHP_EOL);
