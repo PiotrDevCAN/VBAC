972990ca41c8c33381595f52977bf83aac829294
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2024-01-17 15:05:45 +0100

    test emails script

---
+++ cdi_testEmail.php
@@ -0,0 +1,12 @@
+<?php
+use itdq\Trace;
+use itdq\BlueGroups;
+use itdq\BlueMail;
+
+$to = array();
+$to[] = 'piotr.tajanowicz@kyndryl.com';
+
+$replyto = 'test@kyndryl.com';
+$result = BlueMail::send_mail($to, 'Test', '<h1>Testing 1 2 3</h1>', $replyto);
+
+var_dump($result);
